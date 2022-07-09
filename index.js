27eb691fbebc39c8d9522e216c2edf0eeb1ded06
Egidijus Kivilis <egidijus.kivilis@gmail.com>
2022-07-10 00:46:14 +0300

    Sukurti POST, PUT, DELETE

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const {
 const {
   MongoClient,
   ServerApiVersion,
+  ObjectId,
 } = require('mongodb');
 
 const client = new MongoClient(DB_URI, {
@@ -42,11 +43,59 @@ app.get('/users', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  const newUser = req.body;
+  console.log(req.body);
   client.connect(async () => {
     const collection = client.db(DB_NAME).collection(USERS_COLLECTION);
-    const result = await collection.insertOne({ ...newUser });
-    res.json(result);
+    const {
+      first_name, last_name, email, age,
+    } = req.body;
+    const result = await collection.insertOne({
+      first_name,
+      last_name,
+      email,
+      age: Number(age),
+    });
     client.close();
+
+    res.json(result);
+  });
+});
+
+app.delete('/users/:id', (req, res) => {
+  console.log(req.params);
+  client.connect(async () => {
+    const collection = client.db(DB_NAME).collection(USERS_COLLECTION);
+    const result = await collection.deleteOne({
+      _id: ObjectId(req.params.id),
+    });
+    client.close();
+
+    res.json(result);
+  });
+});
+
+app.put('/users', (req, res) => {
+  client.connect(async (err, clientDb) => {
+    if (err) {
+      res.send('Something went wrong!!');
+      clientDb.close();
+    } else {
+      const collection = client.db(DB_NAME).collection(USERS_COLLECTION);
+      const {
+        _id, first_name, last_name, email, age,
+      } = req.body;
+      const filter = { _id: ObjectId(_id) };
+      const newValues = {
+        first_name, last_name, email, age,
+      };
+      try {
+        const result = await collection.replaceOne(filter, newValues);
+        res.send(result);
+        clientDb.close();
+      } catch (error) {
+        res.send('Something went wrong!!');
+        clientDb.close();
+      }
+    }
   });
 });
